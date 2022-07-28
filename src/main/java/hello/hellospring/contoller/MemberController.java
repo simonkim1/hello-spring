e0de41dc47502928328ae5570cf7b70268e15ec8
simonkim1 <sevencuter@gmail.com>
2022-07-28 10:59:37 +0900

    회원 웹기능 1. 홈 화면 추가 2. 등록 3. 조회

--- src/main/java/hello/hellospring/contoller/MemberController.java
+++ src/main/java/hello/hellospring/contoller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.contoller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,34 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    /*
+    * 기본적으로 url을 치는 경우는 GET 방식이며, GET 방식은 주로 조회를 할 때 쓴다.
+    * 데이터를 form 같은것에 넣어서 전달 할 때는 POST를 쓴다. POST 방식은 주로 등록 할 때 쓴다.
+    *
+    * */
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
