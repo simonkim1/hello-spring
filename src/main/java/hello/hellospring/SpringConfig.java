822f1c5783f297c7c132d0af16baaf3411899827
simonkim1 <sevencuter@gmail.com>
2022-08-01 11:40:45 +0900

    Spring DATA JPA Example

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,38 +1,46 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
 
+    // JDBC
 //    private DataSource dataSource;
 //
 //    public SpringConfig(DataSource dataSource) {
 //        this.dataSource = dataSource;
 //    }
 
-    private EntityManager em;
+    // JPA
+//    private EntityManager em;
+//
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    // Spring data JPA
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository(); // Memory에 저장
-//        return new JdbcMemberRepository(dataSource); // 순수한 JDBC
-//        return new JdbcTemplateMemberRepository(dataSource); // JDBC Template를 이용
-        return new JpaMemberRepository(em); // JDBC Template를 이용
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository(); // Memory에 저장
+////        return new JdbcMemberRepository(dataSource); // 순수한 JDBC
+////        return new JdbcTemplateMemberRepository(dataSource); // JDBC Template를 이용
+////        return new JpaMemberRepository(em); // JDBC Template를 이용
+//    }
 }
